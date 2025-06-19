39fd244860a151633625168e22e2a265420edc29
FartInLift <chatdanai.boonsingma@priorsolution.co.th>
2025-06-19 10:13:55 +0700

    showcategories

--- src/app/module/categories/categories.component.ts
+++ src/app/module/categories/categories.component.ts
@@ -4,10 +4,10 @@ import { SharedAppModule } from 'src/app/shared/shared-app.module';
 import { CategoriesService } from './service/categories.service';
 import { Router } from '@angular/router';
 
-export interface Categories{
-  name:string;
-  discription:string;
-  id:number;
+export interface Categories {
+  name: string;
+  description: string;
+  id: number;
 }
 
 @Component({
@@ -24,12 +24,12 @@ export class CategoriesComponent implements OnInit {
   categoriesFind: Categories[] = [];
 
   ngOnInit() {
-      this.findCategories();
+    this.findCategories();
   }
 
   findCategories(){
     this.service.findCategories().subscribe((response:any) =>{
-      this.categoriesFind = response.date;
+      this.categoriesFind = response.data;
       this._cdr.markForCheck();
     })
   }
